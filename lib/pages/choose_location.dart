23906bdac93009f010e71e4a5e2bdc3712001d3b
yasminezegaoui <yasminezegaoui1@gmail.com>
2024-07-28 18:40:56 +0100

    update

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -11,11 +11,28 @@ class _ChooseLocationState extends State<ChooseLocation> {
 
   int counter = 0;
 
+  void getData() async{
+
+    // simulate network request for a username
+    String username = await Future.delayed(const Duration(seconds: 3), (){
+      return 'yasmine';
+    });
+
+    // simulate network request to get bio of the username
+    String bio = await Future.delayed(const Duration(seconds: 2), (){
+      return 'vega, musician,...';
+    });
+
+    // ignore: avoid_print
+    print('$username - $bio');
+  }
+
   @override
   void initState() {
     super.initState();
     // ignore: avoid_print
-    print('initState function ran');
+    // print('initState function ran');
+    getData();
   }
 
   @override
