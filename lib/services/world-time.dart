3b9e9918fd6543eb9840479db5b68e78a3cca3d9
yasminezegaoui <yasminezegaoui1@gmail.com>
2024-07-29 00:26:51 +0100

    update

--- lib/services/world-time.dart
+++ lib/services/world-time.dart
@@ -9,6 +9,7 @@ class WorldTime{
   late String time; // the time in that location
   late String flag; // url to an asset flag icon
   late String url; // location url for api endpoints
+  late bool isDayTime; //true or false
 
   WorldTime({required this.location, required this.flag, required this.url});
 
@@ -32,6 +33,7 @@ class WorldTime{
     now = now.add(Duration(hours: int.parse(offset)));
 
     // set the time property
+    isDayTime = now.hour > 6 && now.hour < 20 ? true : false;
     time = DateFormat.jm().format(now);
     }catch(e){
       // ignore: avoid_print
